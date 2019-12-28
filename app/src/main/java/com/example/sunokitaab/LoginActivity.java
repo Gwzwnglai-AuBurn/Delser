10f0498879a7113c855e805590475af8a09e9741
gwzwn <gwzwnglai@protonmail.com>
2019-12-29 02:19:19 +0530

    intent functions for login activities

--- app/src/main/java/com/example/sunokitaab/LoginActivity.java
+++ app/src/main/java/com/example/sunokitaab/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,16 +17,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void forgotPass() {
+    public void forgotPass(View view) {
         Intent intent = new Intent(this, ForgotPasswordActivity.class);
         EditText editText = (EditText) findViewById(R.id.user_email);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
-    public void signUp() {
+    public void signUp(View view) {
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
 
+    public void login(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
 }
